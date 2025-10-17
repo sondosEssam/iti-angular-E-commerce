1ac17fd87fe97465f560e855b9a549dc91574a34
sondosEssam <sondosessam713@gmail.com>
2025-10-17 20:40:10 +0300

    adding vercel settigns

--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
-import jsonServer from "json-server";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const jsonServer = require("json-server");
+const path = require("path");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 10000;
-
 server.use(middlewares);
-server.use(router);
+server.use("/api", router);
+
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`✅ JSON Server is running on port ${port}`);
+  console.log(`JSON Server is running on port ${port}`);
 });
